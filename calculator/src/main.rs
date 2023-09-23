f042e6cb01f7c83114332ae0788f6be0d0d55458
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-09-23 18:30:47 +0530

    lesson 10

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
     let first_name = "suvan";
+    let name: &str = first_name;
     
-    println!("Hello, {}!", format!("{} {}", first_name, "102321"));
+    println!("Hello, {}!", format!("{} {}", name, "102321"));
 
     let last_name = String::from("gs");
-    println!("Hello, {}!", format!("{} {}", first_name, last_name));
+    println!("Hello, {}!", format!("{} {}", name, last_name));
 
 }
