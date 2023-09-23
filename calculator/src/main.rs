6545f6ab14ae3cc488935f4014cb7f45a78807ed
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-09-23 18:49:10 +0530

    lesson 16 to 20

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,13 +1,16 @@
 fn main() {
-    let first_name = "suvan";
-    let name = String::from(first_name);
+    // let first_name = "suvan";
+    // let name = String::from(first_name);
     
     // println!("Hello, {}!", format!("{} {}", first_name, "102321"));
 
-    let last_name = String::from("gs");
-    let new_name = name.to_owned() + " " + &last_name;
+    // let last_name = String::from("gs");
+    // let new_name = name.to_owned() + " " + &last_name;
     // println!("Hello, {}!", format!("{} {}", name, last_name));
-
-    println!("Hello bro, {}!", format!("{}",  new_name));
+    
+    let mut name2 = String::from("suvan");
+    name2.push_str(" gs");
+    // println!("Hello bro, {}!", format!("{}",  new_name));
+    println!("Hello broski, {}!", format!("{}",  name2));
 
 }
