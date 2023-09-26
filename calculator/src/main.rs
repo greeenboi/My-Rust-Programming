009ce70c50dd3534d524475058f1d46e38110355
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-09-27 01:09:34 +0530

    datatypes cant be changed after declaration

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
     println!("Hello, world!");
     let mut x = 1.1;
-    x=1.12;
     let mut y = 2.2;
-    y=2.22;
     print!("{} + {} = {}", x, y, x + y);
+    x=1.12;
+    y=2.0;
     print!("{} * {} = {}", x, y, x * y);
     // panic!("oh no!!! {}", "something went wrong");
 
