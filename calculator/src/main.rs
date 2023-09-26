412ea84dc4b27d0265eabd8c3fa24439a926f541
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-09-27 01:04:37 +0530

    changed course to frontend masters

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,16 +1,11 @@
 fn main() {
-    // let first_name = "suvan";
-    // let name = String::from(first_name);
-    
-    // println!("Hello, {}!", format!("{} {}", first_name, "102321"));
-
-    // let last_name = String::from("gs");
-    // let new_name = name.to_owned() + " " + &last_name;
-    // println!("Hello, {}!", format!("{} {}", name, last_name));
-    
-    let mut name2 = String::from("suvan");
-    name2.push_str(" gs");
-    // println!("Hello bro, {}!", format!("{}",  new_name));
-    println!("Hello broski, {}!", format!("{}",  name2));
+    println!("Hello, world!");
+    let mut x = 1.1;
+    x=1.12;
+    let mut y = 2.2;
+    y=2.22;
+    print!("{} + {} = {}", x, y, x + y);
+    print!("{} * {} = {}", x, y, x * y);
+    // panic!("oh no!!! {}", "something went wrong");
 
 }
