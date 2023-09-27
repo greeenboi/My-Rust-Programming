f580d1fde24776e8d48fa7b5b82bafa559922c61
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-09-27 08:42:21 +0530

    task 24

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,11 +1,10 @@
 fn main() {
-    println!("Hello, world!");
-    let mut x = 1.1;
-    let mut y = 2.2;
-    print!("{} + {} = {}", x, y, x + y);
-    x=1.12;
-    y=2.0;
-    print!("{} * {} = {}", x, y, x * y);
+
+    let name: &str = "∞";
+    println!(" {} vs {}", name.len() , name.chars().count());
+    
     // panic!("oh no!!! {}", "something went wrong");
+
+   
 
 }
