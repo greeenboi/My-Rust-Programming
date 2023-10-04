e7ca8a1fc6dd87eff876842be3f098e50b78c61f
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-10-05 02:10:41 +0530

    dumped info

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
-
     let name: &str = "∞";
     println!(" {} vs {}", name.len() , name.chars().count());
-    
-    // panic!("oh no!!! {}", "something went wrong");
+    let answer = myfunc(1, 2);
 
-   
-
+    println!("1 / 2 = {}", answer);
 }
+fn myfunc(x: i64, y: u8) -> i64 {
+    return x / (y as i64);
+}
+    
